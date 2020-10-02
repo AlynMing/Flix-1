77b53d0c1850806bb1214dfc485e5f61365d71d7
Mathew Chanda <mvchanda@ncsu.edu>
2020-10-02 01:04:07 -0400

    Finished assignment 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mathew Chanda on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
